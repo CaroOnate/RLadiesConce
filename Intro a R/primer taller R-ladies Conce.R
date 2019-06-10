a2a733607e03b7eba71683344204cf1e478ac64c
CaroLa <41973458+CaroOnate@users.noreply.github.com>
2019-06-10 14:41:24 -0400

    Update primer taller R-ladies Conce.R

--- Intro a R/primer taller R-ladies Conce.R
+++ Intro a R/primer taller R-ladies Conce.R
@@ -1,29 +1,18 @@
-#Hola!! Bienvenid@ al primer taller de R-Ladies Concepción!, queremos que te 
-#sientas cómod@ y con la libertad de plantearnos cualquier tipo de duda, estamos
-#para apoyarte!
 
-#Si tienes el programa R o RStudio instalado en tu ordenador, daremos inicio al
-#procedimiento, en caso contrario, puedes dar aviso a una de las coordinadoras y
-#te ayudaremos con la instalación.
+"'# Hola!! Bienvenid@ al primer taller de R-Ladies ConcepciÃ³n!, queremos que te 
+sientas cÃ³mod@ y con la libertad de plantearnos cualquier tipo de duda, estamos
+para apoyarte!
 
-#Comenzaremos instalando los paquetes que utilizaremos en este primer taller
-#cabe mencionar que con paquetes, nos referimos a las distintas funcionalidades 
-#que posee R para trabajar de diversas maneras los datos.
+Si tienes el programa R o RStudio instalado en tu laptop, daremos inicio al
+procedimiento, en caso contrario, puedes dar aviso a una de las coordinadoras y
+te ayudaremos con la instalaciÃ³n.
 
-#los paquetes a utilizar son gapminder, babynames, tidyverse, es bueno que
-#recordemos que R distingue entre mayúsculas y minúsculas, por ende, debemos
-#ser cuidadosos al escribir los comandos, ahora procedemos a instalar los
-#paquetes ya mencionados:
+Comenzaremos instalando los paquetes que utilizaremos en este primer taller
+cabe mencionar que con paquetes, nos referimos a las distintas funcionalidades 
+que posee R para trabajar de diversas maneras los datos.
 
-install.packages("gapminder")
+los paquetes a utilizar son gapminder, babynames, tidyverse, es bueno que
+recordemos que R distingue entre mayÃºsculas y minÃºsculas, por ende, debemos
+ser cuidadosos al escribir los comandos, ahora procedemos a instalar los
+paquetes ya mencionados: '"
 
-install.packages("babynames")
-#los dos paquetes instalados en las líneas anteriores incluyen datos que 
-#utilizaremos durante el transcurso del taller
-
-install.packages("tidyverse")
-#el paquete instalado en la línea anterior corresponde a un megapaquete,
-#esto quiere decir que dentro de el, existen más paquetes
-
-#procederemos a abrir la librería, osea a activar los paquetes que incluye
-#"tidyverse"
